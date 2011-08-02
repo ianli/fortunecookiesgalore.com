e8682828d029a8b7a12595ca195411c0fd335a6d
Ian Li <ianarli@gmail.com>
2011-08-02 02:24:46 -0400

    Added space from add

--- _layouts/fortune.php
+++ _layouts/fortune.php
@@ -1,13 +1,14 @@
 <? rafting('content'); ?>
 
 	<div id="fortune"><?= raft('fortune'); ?></div>
-
-	<!-- Twitter -->
-	<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal"></a>
-	<!-- Facebook -->
-	<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=250973958259954&amp;xfbml=1"></script><fb:like href="" send="false" layout="button_count" width="90" show_faces="false" font=""></fb:like>
-	<!-- Google +1 -->
-	<g:plusone size="medium"></g:plusone>
+	<div id="fortune-share">
+		<!-- Twitter -->
+		<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal"></a>
+		<!-- Facebook -->
+		<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=250973958259954&amp;xfbml=1"></script><fb:like href="" send="false" layout="button_count" width="90" show_faces="false" font=""></fb:like>
+		<!-- Google +1 -->
+		<g:plusone size="medium"></g:plusone>
+	</div>
 
 <? end_rafting('content'); ?>
 <?php
