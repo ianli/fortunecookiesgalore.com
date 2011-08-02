b6d1f88daa6e1f2ba5e2c79ccc8c60614966f2f9
Ian Li <ianarli@gmail.com>
2011-08-02 03:59:36 -0400

    Added Google Webmasters verification file

--- generate.php
+++ generate.php
@@ -135,6 +135,7 @@
 	generate_fortune_pages($fortunes);
 	generate_fortune_paginations($fortunes);
 	
+	rcopy('google69fca91fe5a019cf.html', '_site/google69fca91fe5a019cf.html');
 	rcopy('css', '_site/css');
 	rcopy('js', '_site/js');
 ?>
