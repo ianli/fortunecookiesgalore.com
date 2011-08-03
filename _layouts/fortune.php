bc9210517be5103e68f0f3d4cfd6550552b39750
Ian Li <ianarli@gmail.com>
2011-08-03 00:16:51 -0400

    Made Facebook Like button asynchronous.

--- _layouts/fortune.php
+++ _layouts/fortune.php
@@ -4,10 +4,12 @@
 	<div id="fortune-share">
 		<!-- Twitter -->
 		<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal"></a>
-		<!-- Facebook -->
-		<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=250973958259954&amp;xfbml=1"></script><fb:like href="" send="false" layout="button_count" width="90" show_faces="false" font=""></fb:like>
 		<!-- Google +1 -->
 		<g:plusone size="medium"></g:plusone>
+		<!-- Facebook -->
+		<div id="fb-root"></div>
+		<!-- <script src="http://connect.facebook.net/en_US/all.js#appId=250973958259954&amp;xfbml=1"></script> -->
+		<fb:like href="" send="false" layout="button_count" width="90" show_faces="false" font=""></fb:like>
 	</div>
 
 <? end_rafting('content'); ?>
--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
-(function() {
-	// Load Twitter button.
-	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-	po.src = 'http://platform.twitter.com/widgets.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-	
-	// Load Google +1 button.
-	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-	po.src = 'https://apis.google.com/js/plusone.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
-
 function goRandomly(count) {
 	location.href = '/' + Math.floor(Math.random() * count);
 }
+
+function loadScript(script) {
+	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	po.src = script;
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+}
+
+// Facebook Async
+// http://developers.facebook.com/docs/reference/javascript/FB.init/
+window.fbAsyncInit = function () {
+	FB.init({
+		appId: '250973958259954',
+		xfbml: true
+	});
+};
+
+// StatCounter variables
+var sc_project = 7101600; 
+var sc_invisible = 1; 
+var sc_security = '0fb08c89';
+
+(function() {
+	// Twitter button.
+	loadScript('http://platform.twitter.com/widgets.js');
+	
+	// Google +1 button.
+	loadScript('https://apis.google.com/js/plusone.js');
+	
+	// Facebook
+	loadScript('http://connect.facebook.net/en_US/all.js');
+	
+	// StatCounter
+	loadScript('http://www.statcounter.com/counter/counter.js');
+})();
